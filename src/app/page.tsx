a127efb5f402b3edcbf2282f278a52bf85b50e63
Abtaha Farooq <abtaha2005@gmail.com>
2025-11-28 17:51:10 +0300

    Fix fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Link from "next/link";
 
 export default function HomePage() {
-  return <h1>Martnetics</h1>;
+  return (
+    <div className="flex flex-col justify-center px-4 tracking-widest text-white">
+      <div className="w-1/2">
+        <div className="text-6xl font-extrabold tracking-tight lg:text-7xl 2xl:text-9xl">
+          Add Another Dimension With Martnetics.
+        </div>
+      </div>
+    </div>
+  );
 }
